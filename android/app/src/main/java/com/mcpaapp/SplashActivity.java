b14d4b6184f129659ae0b5cfefdbbff8a37be04b
selva <selva@codingmart.com>
2021-02-03 10:24:16 +0530

    initial setup with splash screen integration

---
+++ android/app/src/main/java/com/mcpaapp/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.mcpaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+// import android.support.v7.app.AppCompatActivity; //  ← For RN >= 0.60
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mcpaapp.MainActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
